06616a5fd026c22e9c25ab3fdc15f4e4e844816a
SENTHIL-7 <apsenthilkumar2001@gmail.com>
2024-09-01 18:31:54 +0530

    initial page route

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+      <div>
+        <h1>About us page</h1>
+      </div>
+    );
+  }
+  
---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+    return (
+      <div>
+         <h1>Conatact us page</h1>
+      </div>
+    );
+  }
